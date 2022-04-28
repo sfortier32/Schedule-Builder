34516f34eda15f79dc793c8fd43159f801607715
Aryaman Agrawal <aryaman.agrawal21@gmail.com>
2022-04-28 12:45:48 -0400

    Update

--- Schedule Builder/Actual App Code/Home/Settings.swift
+++ Schedule Builder/Actual App Code/Home/Settings.swift
@@ -18,6 +18,12 @@ struct Settings: View {
     @State private var showView = "Settings"
     @State private var email = ""
     @State private var password = ""
+    @State private var majors = ""
+    @State private var minors = ""
+    @State private var certificates = ""
+    @State private var credits_completed = ""
+    @State private var credits_transfered = ""
+    @State private var credits_needed = ""
     @StateObject var global = GlobalVariables()
     
     
@@ -25,7 +31,6 @@ struct Settings: View {
         switch showView {
         case "Settings":
             ZStack {
-                
                 Background()
                 LinearGradient(gradient: Gradient(colors: [.blue1, .blue2]), startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
                     .mask {
@@ -38,13 +43,15 @@ struct Settings: View {
                             VStack{
                                 Text("Personal")
                                     .font(.custom("Inter-Bold", size: 20))
-                                    .foregroundColor(Color("recWhite"))
-                                    .multilineTextAlignment(TextAlignment.leading)
+                                    .foregroundColor(Color("almostBlack")).frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(EdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 0))
                                 
                                 Text("Email")
                                     .font(.custom("Inter-Medium", size: 18))
-                                    .foregroundColor(Color("recWhite"))
+                                    .foregroundColor(Color("almostBlack"))
                                     .multilineTextAlignment(TextAlignment.leading)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(EdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 0))
                                 
                                 RoundedRectangle(cornerRadius: 20)
                                     .strokeBorder(Color.white, lineWidth: 3)
@@ -60,8 +67,10 @@ struct Settings: View {
                                 
                                 Text("Password")
                                     .font(.custom("Inter-Medium", size: 18))
-                                    .foregroundColor(Color("recWhite"))
+                                    .foregroundColor(Color("almostBlack"))
                                     .multilineTextAlignment(TextAlignment.leading)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(EdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 0))
                                 
                                 RoundedRectangle(cornerRadius: 20)
                                     .strokeBorder(Color.white, lineWidth: 3)
@@ -77,12 +86,17 @@ struct Settings: View {
                                 
                                 Text("Academics")
                                     .font(.custom("Inter-Bold", size: 20))
-                                    .foregroundColor(Color("recWhite"))
+                                    .foregroundColor(Color("almostBlack"))
                                     .multilineTextAlignment(TextAlignment.leading)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(EdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 0))
+                                
                                 Text("Major(s)")
                                     .font(.custom("Inter-Medium", size: 18))
-                                    .foregroundColor(Color("recWhite"))
+                                    .foregroundColor(Color("almostBlack"))
                                     .multilineTextAlignment(TextAlignment.leading)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(EdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 0))
                                 
                                 RoundedRectangle(cornerRadius: 20)
                                     .strokeBorder(Color.white, lineWidth: 3)
@@ -100,15 +114,16 @@ struct Settings: View {
                                     VStack{
                                         Text("Minor(s)")
                                             .font(.custom("Inter-Medium", size: 18))
-                                            .foregroundColor(Color("recWhite"))
+                                            .foregroundColor(Color("almostBlack"))
                                             .multilineTextAlignment(TextAlignment.leading)
+                                        
                                         RoundedRectangle(cornerRadius: 20)
                                             .strokeBorder(Color.white, lineWidth: 3)
                                             .frame(width: 175, height: 48)
                                             .background(Color.white.opacity(1))
-                                            .cornerRadius(20)
+                                            .cornerRadius(30)
                                             .overlay (
-                                                TextField("Major", text: $email)
+                                                TextField("Minor", text: $email)
                                                     .font(.custom("Inter-Medium", size: 16))
                                                     .foregroundColor(.blueGray)
                                                     .padding(sides: [.left, .right], value: 20)
@@ -118,13 +133,13 @@ struct Settings: View {
                                     VStack{
                                         Text("Certificate(s)")
                                             .font(.custom("Inter-Medium", size: 18))
-                                            .foregroundColor(Color("recWhite"))
+                                            .foregroundColor(Color("almostBlack"))
                                             .multilineTextAlignment(TextAlignment.leading)
                                         RoundedRectangle(cornerRadius: 20)
                                             .strokeBorder(Color.white, lineWidth: 3)
                                             .frame(width: 175, height: 48)
                                             .background(Color.white.opacity(1))
-                                            .cornerRadius(20)
+                                            .cornerRadius(30)
                                             .overlay (
                                                 TextField("Certificate", text: $email)
                                                     .font(.custom("Inter-Medium", size: 16))
@@ -138,34 +153,37 @@ struct Settings: View {
                                     VStack{
                                         Text("Credits\nCompleted")
                                             .font(.custom("Inter-Medium", size: 18))
-                                            .foregroundColor(Color("recWhite"))
+                                            .foregroundColor(Color("almostBlack"))
                                             .multilineTextAlignment(TextAlignment.leading)
                                         RoundedRectangle(cornerRadius: 20)
                                             .strokeBorder(Color.white, lineWidth: 3)
                                             .frame(width: 100, height: 48)
                                             .background(Color.white.opacity(1))
-                                            .cornerRadius(20)
+                                            .cornerRadius(30)
                                             .overlay (
                                                 TextField("50", text: $email)
                                                     .font(.custom("Inter-Medium", size: 30))
                                                     .foregroundColor(.black)
+                                                    .multilineTextAlignment(.center)
                                                     .padding(sides: [.left, .right], value: 20)
-                                            ).padding(.bottom, 10).padding(sides: [.left, .right], value: 20)
+                                            ).padding(.bottom, 10).padding(sides: [.left, .right], value: 10)
                                     }
                                     
                                     VStack{
                                         Text("Credits\nTransfered")
                                             .font(.custom("Inter-Medium", size: 18))
-                                            .foregroundColor(Color("recWhite"))
+                                            .foregroundColor(Color("almostBlack"))
                                             .multilineTextAlignment(TextAlignment.leading)
+                                        
                                         RoundedRectangle(cornerRadius: 20)
                                             .strokeBorder(Color.white, lineWidth: 3)
                                             .frame(width: 100, height: 48)
                                             .background(Color.white.opacity(1))
-                                            .cornerRadius(20)
+                                            .cornerRadius(30)
                                             .overlay (
                                                 TextField("50", text: $email)
                                                     .font(.custom("Inter-Medium", size: 30))
+                                                    .multilineTextAlignment(.center)
                                                     .foregroundColor(.black)
                                                     .padding(sides: [.left, .right], value: 20)
                                             ).padding(.bottom, 10)
@@ -174,30 +192,32 @@ struct Settings: View {
                                     VStack{
                                         Text("Credits\nNeeded")
                                             .font(.custom("Inter-Medium", size: 18))
-                                            .foregroundColor(Color("recWhite"))
+                                            .foregroundColor(Color("almostBlack"))
                                             .multilineTextAlignment(TextAlignment.leading)
                                         RoundedRectangle(cornerRadius: 20)
                                             .strokeBorder(Color.white, lineWidth: 3)
                                             .frame(width: 100, height: 48)
                                             .background(Color.white.opacity(1))
-                                            .cornerRadius(20)
+                                            .cornerRadius(30)
                                             .overlay (
                                                 TextField("50", text: $email)
                                                     .font(.custom("Inter-Medium", size: 30))
-                                                    .foregroundColor(.black)
+                                                    .multilineTextAlignment(.center)
+                                                    .foregroundColor(Color.black)
                                                     .padding(sides: [.left, .right], value: 20)
                                             ).padding(.bottom, 10)
                                     }
-                                }                                                                
-                                
+                                                                                                            
+                                }
+                                                
                             }.position(x: 207, y: 200).padding(.top, 80)
+                            
+                            
                         }.position(x: 207, y: 350).frame(width: UIScreen.main.bounds.size.width, height: 590, alignment: Alignment.center)
                     )
                 
-                
                 VStack() {
                     HStack {
-                        
                         Text("Settings")
                             .font(.custom("Inter-Semibold", size: 33))
                             .foregroundColor(Color("recWhite"))
@@ -224,8 +244,6 @@ struct Settings: View {
                         .padding(sides: [.left, .right], value: 30)
                     
                 }.position(x: 207, y: 60)
-                
-                
                 
                 Rectangle()
                     .fill(Color.navBarGray)
