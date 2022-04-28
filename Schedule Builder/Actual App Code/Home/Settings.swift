d5142bc2aeabcc68941c13dbbfa4815f41011191
Aryaman Agrawal <aryaman.agrawal21@gmail.com>
2022-04-28 00:06:20 -0400

    Settings Update

--- Schedule Builder/Actual App Code/Home/Settings.swift
+++ Schedule Builder/Actual App Code/Home/Settings.swift
@@ -16,11 +16,16 @@ struct Settings_Preview: PreviewProvider {
 struct Settings: View {
     
     @State private var showView = "Settings"
+    @State private var email = ""
+    @State private var password = ""
+    @StateObject var global = GlobalVariables()
+    
     
     var body: some View {
         switch showView {
         case "Settings":
             ZStack {
+                Background()
                 LinearGradient(gradient: Gradient(colors: [.blue1, .blue2]), startPoint: .topLeading, endPoint: .bottomTrailing).edgesIgnoringSafeArea(.all)
                     .mask {
                         Rectangle()
@@ -28,16 +33,193 @@ struct Settings: View {
                     }
                     .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: -5)
                 
-                HStack {
-                    VStack() {
+                
+                VStack() {
+                    HStack {
+                        
                         Text("Settings")
                             .font(.custom("Inter-Semibold", size: 33))
-                            .foregroundColor(Color("almostBlack"))
+                            .foregroundColor(Color("recWhite"))
                         Spacer()
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            global.showView = "Launch"
+                        }) {
+                            HStack {
+                                Text("Log Out")
+                                    .font(.custom("Inter-SemiBold", size: 16))
+                                    .foregroundColor(.white)
+                                Image(systemName: "rectangle.portrait.and.arrow.right")
+                                    .resizable()
+                                    .frame(width: 18, height: 16)
+                                    .foregroundColor(.white)
+                                    .padding(sides: [.right], value: 6)
+                            }
+                            
+                        }
+                    }.padding(.top, 80)
+                        .padding(sides: [.left, .right], value: 30)
+                                        
+                }.position(x: 207, y: 60)
+                    
+                VStack{
+                    Text("Personal")
+                        .font(.custom("Inter-Bold", size: 20))
+                        .foregroundColor(Color("recWhite"))
+                        .multilineTextAlignment(TextAlignment.leading)
+                    
+                    Text("Email")
+                        .font(.custom("Inter-Medium", size: 18))
+                        .foregroundColor(Color("recWhite"))
+                        .multilineTextAlignment(TextAlignment.leading)
+                    
+                    RoundedRectangle(cornerRadius: 20)
+                        .strokeBorder(Color.white, lineWidth: 3)
+                        .frame(width: 350, height: 48)
+                        .background(Color.white.opacity(1))
+                        .cornerRadius(20)
+                        .overlay (
+                            TextField("Email", text: $email)
+                                .font(.custom("Inter-Medium", size: 16))
+                                .foregroundColor(.blueGray)
+                                .padding(sides: [.left, .right], value: 20)
+                        ).padding(.bottom, 0)
+                    
+                    Text("Password")
+                        .font(.custom("Inter-Medium", size: 18))
+                        .foregroundColor(Color("recWhite"))
+                        .multilineTextAlignment(TextAlignment.leading)
+                    
+                    RoundedRectangle(cornerRadius: 20)
+                        .strokeBorder(Color.white, lineWidth: 3)
+                        .frame(width: 350, height: 48)
+                        .background(Color.white.opacity(1))
+                        .cornerRadius(20)
+                        .overlay (
+                            TextField("Password", text: $email)
+                                .font(.custom("Inter-Medium", size: 16))
+                                .foregroundColor(.blueGray)
+                                .padding(sides: [.left, .right], value: 20)
+                        ).padding(.bottom, 10)
+                    
+                    Text("Academics")
+                        .font(.custom("Inter-Bold", size: 20))
+                        .foregroundColor(Color("recWhite"))
+                        .multilineTextAlignment(TextAlignment.leading)
+                    Text("Major(s)")
+                        .font(.custom("Inter-Medium", size: 18))
+                        .foregroundColor(Color("recWhite"))
+                        .multilineTextAlignment(TextAlignment.leading)
+                    
+                    RoundedRectangle(cornerRadius: 20)
+                        .strokeBorder(Color.white, lineWidth: 3)
+                        .frame(width: 350, height: 48)
+                        .background(Color.white.opacity(1))
+                        .cornerRadius(20)
+                        .overlay (
+                            TextField("Major", text: $email)
+                                .font(.custom("Inter-Medium", size: 16))
+                                .foregroundColor(.blueGray)
+                                .padding(sides: [.left, .right], value: 20)
+                        ).padding(.bottom, 5)
+                    
+                    HStack{
+                        VStack{
+                            Text("Minor(s)")
+                                .font(.custom("Inter-Medium", size: 18))
+                                .foregroundColor(Color("recWhite"))
+                                .multilineTextAlignment(TextAlignment.leading)
+                            RoundedRectangle(cornerRadius: 20)
+                                .strokeBorder(Color.white, lineWidth: 3)
+                                .frame(width: 175, height: 48)
+                                .background(Color.white.opacity(1))
+                                .cornerRadius(20)
+                                .overlay (
+                                    TextField("Major", text: $email)
+                                        .font(.custom("Inter-Medium", size: 16))
+                                        .foregroundColor(.blueGray)
+                                        .padding(sides: [.left, .right], value: 20)
+                                ).padding(.bottom, 10).padding(sides: [.left, .right], value: 20)
+                        }
+                        
+                        VStack{
+                            Text("Certificate(s)")
+                                .font(.custom("Inter-Medium", size: 18))
+                                .foregroundColor(Color("recWhite"))
+                                .multilineTextAlignment(TextAlignment.leading)
+                            RoundedRectangle(cornerRadius: 20)
+                                .strokeBorder(Color.white, lineWidth: 3)
+                                .frame(width: 175, height: 48)
+                                .background(Color.white.opacity(1))
+                                .cornerRadius(20)
+                                .overlay (
+                                    TextField("Certificate", text: $email)
+                                        .font(.custom("Inter-Medium", size: 16))
+                                        .foregroundColor(.blueGray)
+                                        .padding(sides: [.left, .right], value: 20)
+                                ).padding(.bottom, 10)
+                        }
                     }
-                    Spacer()
-                }.padding(.top, 80)
-                    .padding(sides: [.left, .right], value: 30)
+                    
+                    HStack{
+                        VStack{
+                            Text("Credits\nCompleted")
+                                .font(.custom("Inter-Medium", size: 18))
+                                .foregroundColor(Color("recWhite"))
+                                .multilineTextAlignment(TextAlignment.leading)
+                            RoundedRectangle(cornerRadius: 20)
+                                .strokeBorder(Color.white, lineWidth: 3)
+                                .frame(width: 100, height: 48)
+                                .background(Color.white.opacity(1))
+                                .cornerRadius(20)
+                                .overlay (
+                                    TextField("50", text: $email)
+                                        .font(.custom("Inter-Medium", size: 30))
+                                        .foregroundColor(.black)
+                                        .padding(sides: [.left, .right], value: 20)
+                                ).padding(.bottom, 10).padding(sides: [.left, .right], value: 20)
+                        }
+                        
+                        VStack{
+                            Text("Credits\nTransfered")
+                                .font(.custom("Inter-Medium", size: 18))
+                                .foregroundColor(Color("recWhite"))
+                                .multilineTextAlignment(TextAlignment.leading)
+                            RoundedRectangle(cornerRadius: 20)
+                                .strokeBorder(Color.white, lineWidth: 3)
+                                .frame(width: 100, height: 48)
+                                .background(Color.white.opacity(1))
+                                .cornerRadius(20)
+                                .overlay (
+                                    TextField("50", text: $email)
+                                        .font(.custom("Inter-Medium", size: 30))
+                                        .foregroundColor(.black)
+                                        .padding(sides: [.left, .right], value: 20)
+                                ).padding(.bottom, 10)
+                        }
+                        
+                        VStack{
+                            Text("Credits\nNeeded")
+                                .font(.custom("Inter-Medium", size: 18))
+                                .foregroundColor(Color("recWhite"))
+                                .multilineTextAlignment(TextAlignment.leading)
+                            RoundedRectangle(cornerRadius: 20)
+                                .strokeBorder(Color.white, lineWidth: 3)
+                                .frame(width: 100, height: 48)
+                                .background(Color.white.opacity(1))
+                                .cornerRadius(20)
+                                .overlay (
+                                    TextField("50", text: $email)
+                                        .font(.custom("Inter-Medium", size: 30))
+                                        .foregroundColor(.black)
+                                        .padding(sides: [.left, .right], value: 20)
+                                ).padding(.bottom, 10)
+                        }
+                    }                    
+                    
+                }.position(x: 207, y: 280).padding(.top, 60)
                 
                 Rectangle()
                     .fill(Color.navBarGray)
